f19ba25ddb1b41bc129c7ed1a36710c546667233
ahishawR <49690298+ahishawR@users.noreply.github.com>
2022-10-30 16:04:01 -0400

    *to+pattern
    a change from * to +

--- array/pattern.cpp
+++ array/pattern.cpp
@@ -7,7 +7,7 @@ int main(){
         for(j=1;j<=5;j++){
             
             if(i==1||i==5||j==1||j==5){
-                cout<<"*";
+                cout<<"+";
                 //printf("* ");
             }
             else{
