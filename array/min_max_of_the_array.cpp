29a9f69dc1f49250c843c161e0cb210c60d81973
ahishawR <49690298+ahishawR@users.noreply.github.com>
2022-10-31 14:05:35 -0400

    alpha
    alpha

--- array/min_max_of_the_array.cpp
+++ array/min_max_of_the_array.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a[]={7,6,5,4,3,2,5};
+    int a[]={7,6,5,4,3,2,5};   //need to work more
     int min=a[0];
     for(int i=1;i<6;i++){
         if(a[i]<min)
