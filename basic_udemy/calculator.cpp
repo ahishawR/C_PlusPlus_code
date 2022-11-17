35c166b2e7930bf11f28cc41086ccc3bf76faa04
ahishawR <49690298+ahishawR@users.noreply.github.com>
2022-11-17 09:43:19 -0500

    string
    string and loops

---
+++ basic_udemy/calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+     int num1,num2;
+     char opera;
+     cout<<"enter 1st number \n";
+     cin>>num1;
+     cout<<"enter 2nd number \n";
+     cin>>num2;
+     cout<<"enter operator \n";
+     cin>>opera;
+
+    if(opera=='+')
+    cout<<(num1+num2);
+    else if(opera=='-')
+    cout<<(num1-num2);
+    else if(opera=='*')
+    cout<<(num1*num2);
+    else if(opera=='/')
+    cout<<(num1/num2);
+    else
+    cout<<"sorry!!";
+    return 0;
+}
--- loops/multipication_table.cpp
+++ loops/multipication_table.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i=1;
-    while(true){
-        cout<<i++;
+        cout<<"Enter Number ";
+        int num;
+        cin>>num;
+        cout<<endl<<"multipication table of"<<num<<"\n";
+        for(int i=1;i<11;i++){
+            cout<<num<<"*"<<i<<"="<<(num*i)<<endl;
+
+        }
     }
-    return 0;
-}
+
